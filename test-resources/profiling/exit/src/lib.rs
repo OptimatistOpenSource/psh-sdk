79eeb2816d9859d8a5a0da35aee34ffe059a247d
Thaumy <thaumy.cheng@optimatist.com>
2024-01-08 17:22:47 +0800

    fix tests

--- test-resources/profiling/exit/src/lib.rs
+++ test-resources/profiling/exit/src/lib.rs
@@ -1,11 +1,11 @@
 #![cfg(target_arch = "wasm32")]
 #![no_std]
 
-use profiling::op;
+use profiling::intrinsics;
 
 #[profiling::main]
 fn main() {
-    op::log("0");
-    op::exit();
-    op::log("1");
+    intrinsics::log("0");
+    intrinsics::exit();
+    intrinsics::log("1");
 }

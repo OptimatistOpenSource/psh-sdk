a93924527681446e5ebf67dd5c2d71c336741818
Thaumy <thaumy@outlook.com>
2024-02-04 14:08:45 +0800

    move `run_profiling`

--- runtime/src/profiling/runtime/mod.rs
+++ runtime/src/profiling/runtime/mod.rs
@@ -1,11 +1,12 @@
 mod data;
+mod run_profiling;
 
 use std::borrow::Borrow;
 
 pub use data::*;
 
 use anyhow::Result;
-use wasmtime::{Config, Engine, IntoFunc, Linker, Module, Store};
+use wasmtime::{Config, Engine, IntoFunc, Linker};
 
 use crate::profiling::Profiling;
 
@@ -51,47 +52,6 @@ impl ProfilingRuntime {
     }
 
     pub fn run_profiling(&self, profiling: impl Borrow<Profiling>) -> (Data, Result<()>) {
-        /*
-        TODO: Refactor with try block
-        Waiting for feature: https://github.com/rust-lang/rust/issues/31436
-        */
-        fn inner(
-            se: &ProfilingRuntime,
-            profiling: &Profiling,
-        ) -> Result<Data, (Data, anyhow::Error)> {
-            let wasm_module = if profiling.is_aot {
-                unsafe { Module::deserialize(&se.wasm_engine, &profiling.bytes) }
-            } else {
-                Module::new(&se.wasm_engine, &profiling.bytes)
-            }
-            .map_err(|e| (Data::new(), e))?;
-
-            let mut wasm_store = {
-                let data = Data::new(); // for log storage
-                let mut store = Store::new(&se.wasm_engine, data);
-                store.set_epoch_deadline(1);
-                store
-            };
-
-            let result = se
-                .wasm_linker
-                .instantiate(&mut wasm_store, &wasm_module)
-                .map_err(|e| (Data::new(), e))?
-                .get_typed_func::<(), ()>(&mut wasm_store, "main")
-                .map_err(|e| (Data::new(), e))?
-                .call(&mut wasm_store, ());
-
-            let data = wasm_store.into_data();
-            match result {
-                Ok(_) => Ok(data),
-                Err(e) => Err((data, e)),
-            }
-        }
-
-        let profiling = profiling.borrow();
-        match inner(self, profiling) {
-            Ok(data) => (data, Ok(())),
-            Err((data, e)) => (data, Err(e)),
-        }
+        run_profiling::run_profiling(self, profiling)
     }
 }

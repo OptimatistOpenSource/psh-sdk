48881858e8f3f7e12a05c353bb8922213a4d5558
Thaumy <thaumy@outlook.com>
2024-02-04 09:41:37 +0800

    use smaller unsafe blocks for file ops

--- runtime/src/op/file/mod.rs
+++ runtime/src/op/file/mod.rs
@@ -11,10 +11,9 @@ use wasmtime::Caller;
 
 pub fn is_exist(mut caller: Caller<Data>, path_vm_ptr: u32, path_len: u32) -> u32 {
     let caller = &mut caller;
-    unsafe {
-        let path = get_str(caller, path_vm_ptr, path_len);
-        raw::is_exist(path) as _
-    }
+
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len) };
+    raw::is_exist(path) as _
 }
 
 pub fn read(mut caller: Caller<Data>, ret_area_vm_ptr: u32, path_vm_ptr: u32, path_len: u32) {
@@ -71,19 +70,20 @@ pub fn write(
     contents_len: u32,
 ) {
     let caller = &mut caller;
-    unsafe {
-        let ret_area = &mut *(to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3]);
-        let path = get_str(caller, path_vm_ptr, path_len).to_string();
-        let contents = get_str(caller, contents_vm_ptr, contents_len);
-        match raw::write(&path, contents) {
-            Ok(_) => {
-                ret_area[0] = 1;
-            }
-            Err(e) => {
-                let e = e.to_string();
-                let vm_ptr = copy_to_vm(caller, e.as_str());
-                *ret_area = [0, vm_ptr, e.len() as _];
-            }
+
+    let ret_area_ptr = unsafe { to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3] };
+    let ret_area = unsafe { &mut *ret_area_ptr };
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len).to_string() };
+    let contents = unsafe { get_str(caller, contents_vm_ptr, contents_len) };
+
+    match raw::write(&path, contents) {
+        Ok(_) => {
+            ret_area[0] = 1;
+        }
+        Err(e) => {
+            let e = e.to_string();
+            let vm_ptr = unsafe { copy_to_vm(caller, e.as_str()) };
+            *ret_area = [0, vm_ptr, e.len() as _];
         }
     }
 }
@@ -97,19 +97,20 @@ pub fn append(
     contents_len: u32,
 ) {
     let caller = &mut caller;
-    unsafe {
-        let ret_area = &mut *(to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3]);
-        let path = get_str(caller, path_vm_ptr, path_len).to_string();
-        let contents = get_str(caller, contents_vm_ptr, contents_len);
-        match raw::append(&path, contents) {
-            Ok(_) => {
-                ret_area[0] = 1;
-            }
-            Err(e) => {
-                let e = e.to_string();
-                let vm_ptr = copy_to_vm(caller, e.as_str());
-                *ret_area = [0, vm_ptr, e.len() as _];
-            }
+
+    let ret_area_ptr = unsafe { to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3] };
+    let ret_area = unsafe { &mut *ret_area_ptr };
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len).to_string() };
+    let contents = unsafe { get_str(caller, contents_vm_ptr, contents_len) };
+
+    match raw::append(&path, contents) {
+        Ok(_) => {
+            ret_area[0] = 1;
+        }
+        Err(e) => {
+            let e = e.to_string();
+            let vm_ptr = unsafe { copy_to_vm(caller, e.as_str()) };
+            *ret_area = [0, vm_ptr, e.len() as _];
         }
     }
 }
@@ -121,54 +122,57 @@ pub fn remove_file(
     path_len: u32,
 ) {
     let caller = &mut caller;
-    unsafe {
-        let ret_area = &mut *(to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3]);
-        let path = get_str(caller, path_vm_ptr, path_len).to_string();
-        match raw::remove_file(&path) {
-            Ok(_) => {
-                ret_area[0] = 1;
-            }
-            Err(e) => {
-                let e = e.to_string();
-                let vm_ptr = copy_to_vm(caller, e.as_str());
-                *ret_area = [0, vm_ptr, e.len() as _];
-            }
+
+    let ret_area_ptr = unsafe { to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3] };
+    let ret_area = unsafe { &mut *ret_area_ptr };
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len) };
+
+    match raw::remove_file(path) {
+        Ok(_) => {
+            ret_area[0] = 1;
+        }
+        Err(e) => {
+            let e = e.to_string();
+            let vm_ptr = unsafe { copy_to_vm(caller, e.as_str()) };
+            *ret_area = [0, vm_ptr, e.len() as _];
         }
     }
 }
 
 pub fn create_dir(mut caller: Caller<Data>, ret_area_vm_ptr: u32, path_vm_ptr: u32, path_len: u32) {
     let caller = &mut caller;
-    unsafe {
-        let ret_area = &mut *(to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3]);
-        let path = get_str(caller, path_vm_ptr, path_len).to_string();
-        match raw::create_dir(&path) {
-            Ok(_) => {
-                ret_area[0] = 1;
-            }
-            Err(e) => {
-                let e = e.to_string();
-                let vm_ptr = copy_to_vm(caller, e.as_str());
-                *ret_area = [0, vm_ptr, e.len() as _];
-            }
+
+    let ret_area_ptr = unsafe { to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3] };
+    let ret_area = unsafe { &mut *ret_area_ptr };
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len) };
+
+    match raw::create_dir(path) {
+        Ok(_) => {
+            ret_area[0] = 1;
+        }
+        Err(e) => {
+            let e = e.to_string();
+            let vm_ptr = unsafe { copy_to_vm(caller, e.as_str()) };
+            *ret_area = [0, vm_ptr, e.len() as _];
         }
     }
 }
 
 pub fn remove_dir(mut caller: Caller<Data>, ret_area_vm_ptr: u32, path_vm_ptr: u32, path_len: u32) {
     let caller = &mut caller;
-    unsafe {
-        let ret_area = &mut *(to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3]);
-        let path = get_str(caller, path_vm_ptr, path_len).to_string();
-        match raw::remove_dir(&path) {
-            Ok(_) => {
-                ret_area[0] = 1;
-            }
-            Err(e) => {
-                let e = e.to_string();
-                let vm_ptr = copy_to_vm(caller, e.as_str());
-                *ret_area = [0, vm_ptr, e.len() as _];
-            }
+
+    let ret_area_ptr = unsafe { to_host_ptr(caller, ret_area_vm_ptr) as *mut [u32; 3] };
+    let ret_area = unsafe { &mut *ret_area_ptr };
+    let path = unsafe { get_str(caller, path_vm_ptr, path_len) };
+
+    match raw::remove_dir(&path) {
+        Ok(_) => {
+            ret_area[0] = 1;
+        }
+        Err(e) => {
+            let e = e.to_string();
+            let vm_ptr = unsafe { copy_to_vm(caller, e.as_str()) };
+            *ret_area = [0, vm_ptr, e.len() as _];
         }
     }
 }

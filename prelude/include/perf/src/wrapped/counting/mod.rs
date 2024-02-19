e795b5360e8636c43d06f98e0d8ceedac75435e6
Thaumy <thaumy@outlook.com>
2024-02-19 16:17:59 +0800

    add `CounterGroupStat`

--- prelude/include/perf/src/wrapped/counting/mod.rs
+++ prelude/include/perf/src/wrapped/counting/mod.rs
@@ -2,9 +2,11 @@ mod counter;
 mod counter_group;
 mod counter_guard;
 mod fixed_counter_group;
+mod stat;
 
 pub use counter::*;
 pub use counter_group::*;
 pub use counter_guard::*;
 pub use fixed_counter_group::*;
 pub use profiling_prelude_perf_types::counting::*;
+pub use stat::*;
--- prelude/include/perf/types/src/counting/stat.rs
+++ prelude/include/perf/types/src/counting/stat.rs
@@ -1,3 +1,4 @@
+use alloc::collections::BTreeMap;
 use rkyv::{Archive, Deserialize, Serialize};
 
 #[derive(Archive, Deserialize, Serialize, PartialEq, Eq, Debug, Clone)]
@@ -6,4 +7,12 @@ pub struct CounterStat {
     pub event_count: u64,
     pub time_enabled: u64,
     pub time_running: u64,
+}
+
+#[derive(Archive, Deserialize, Serialize, PartialEq, Eq, Debug, Clone)]
+pub struct CounterGroupStat {
+    pub time_enabled: u64,
+    pub time_running: u64,
+    /// Map of `event_id` -> `event_count`
+    pub member_counts: BTreeMap<u64, u64>,
 }

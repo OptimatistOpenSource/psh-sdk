5271759293273d08aacfb75aef40d0870fafbd36
Thaumy <thaumy@outlook.com>
2024-02-19 15:47:49 +0800

    add `CounterGroup`

--- prelude/include/perf/src/wrapped/counting/mod.rs
+++ prelude/include/perf/src/wrapped/counting/mod.rs
@@ -1,6 +1,8 @@
 mod counter;
+mod counter_group;
 mod fixed_counter_group;
 
 pub use counter::*;
+pub use counter_group::*;
 pub use fixed_counter_group::*;
 pub use profiling_prelude_perf_types::counting::*;

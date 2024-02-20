7348b93f2ee5c1e0ef69dccbe9e2edab7e861944
Thaumy <thaumy@outlook.com>
2024-02-20 08:58:02 +0800

    rename fileds for `ProfilingRuntime`
    - rename `wasm_engine` to `engine`
    - rename `wasm_linker` to `linker`

--- runtime/src/profiling/runtime/mod.rs
+++ runtime/src/profiling/runtime/mod.rs
@@ -11,8 +11,8 @@ use wasmtime::{Config, Engine, IntoFunc, Linker};
 use crate::profiling::Profiling;
 
 pub struct ProfilingRuntime {
-    pub(crate) wasm_engine: Box<Engine>,
-    pub(crate) wasm_linker: Linker<Data>,
+    pub(crate) engine: Box<Engine>,
+    pub(crate) linker: Linker<Data>,
 }
 
 impl Default for ProfilingRuntime {
@@ -23,16 +23,14 @@ impl Default for ProfilingRuntime {
 
 impl ProfilingRuntime {
     pub fn new() -> Self {
-        let wasm_engine = {
+        let engine = {
             let mut cfg = Config::new();
             cfg.epoch_interruption(true);
             let engine = Engine::new(&cfg).unwrap();
             Box::new(engine)
         };
-        Self {
-            wasm_linker: Linker::new(wasm_engine.as_ref()),
-            wasm_engine,
-        }
+        let linker = Linker::new(&engine);
+        Self { linker, engine }
     }
 
     pub fn precompile_profiling(&self, profiling: Profiling) -> Result<Profiling> {
@@ -40,14 +38,14 @@ impl ProfilingRuntime {
             return Ok(profiling);
         }
 
-        let precompiled_bytes = self.wasm_engine.precompile_module(&profiling.bytes)?;
+        let precompiled_bytes = self.engine.precompile_module(&profiling.bytes)?;
 
         let profiling = unsafe { Profiling::from_precompiled(precompiled_bytes) };
         Ok(profiling)
     }
 
     pub fn link_op<P, R>(&mut self, name: &str, f: impl IntoFunc<Data, P, R>) -> Result<&mut Self> {
-        self.wasm_linker.func_wrap("op", name, f)?;
+        self.linker.func_wrap("op", name, f)?;
         Ok(self)
     }
 

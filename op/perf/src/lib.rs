14920368bdbc181db44ff027cd10e38aea5fd425
Thaumy <thaumy@outlook.com>
2024-03-05 11:39:41 +0800

    add `PerfCtx` and `PerfView`

--- op/perf/src/lib.rs
+++ op/perf/src/lib.rs
@@ -1,5 +1,25 @@
+use wasmtime::component::{Linker, ResourceTable};
+
 pub mod convert;
 pub mod counting;
+
 #[cfg(test)]
 mod tests;
 
+pub struct PerfCtx {
+    // TODO
+}
+
+#[allow(clippy::new_without_default)]
+impl PerfCtx {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+pub trait PerfView {
+    fn table(&self) -> &ResourceTable;
+    fn table_mut(&mut self) -> &mut ResourceTable;
+    fn ctx(&self) -> &PerfCtx;
+    fn ctx_mut(&mut self) -> &mut PerfCtx;
+}
